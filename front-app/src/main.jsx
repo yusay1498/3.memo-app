5263d24d244f9df46acfd5205dd13e15a9a64332
yusay1498 <u.yusay1498@gmail.com>
2025-05-06 17:51:28 +0900

    add sass module and create main.scss

--- front-app/src/main.jsx
+++ front-app/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './pages/App.jsx'
+import './assets/styles/main.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
