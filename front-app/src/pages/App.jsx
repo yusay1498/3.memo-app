3309d99fd5c0155372259b90e0185fa4d7a20b02
yusay1498 <u.yusay1498@gmail.com>
2025-05-07 15:19:17 +0900

    add base styles

--- front-app/src/pages/App.jsx
+++ front-app/src/pages/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import MemoInput from '../components/MemoInput';
 import MemoList from '../components/MemoList';
+import titleImg from '../assets/images/memo.png';
 
 function App() {
     const [memos, setMemos] = useState(() => {
@@ -21,8 +22,10 @@ function App() {
     };
 
     return (
-        <div>
-            <h1>Memo App</h1>
+        <div className="wrap">
+            <header id="header">
+                <img className="title" src={titleImg} alt="memo app title" />
+            </header>
             <MemoInput onAdd={addMemo} />
             <MemoList memos={memos} onDelete={deleteMemo} />
         </div>
